15a61daba94ae29b8bfdf19d965d95d6cd017901
= <ronaldo.alves.rios@gmail.com>
2022-03-25 16:13:34 -0300

    Artefatos iniciais do projeto

---
+++ src/main/java/br/com/tcs/hbdelivery/HbeerdeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.tcs.hbdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbeerdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbeerdeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tcs/hbdelivery/HbeerdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tcs.hbdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbeerdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
